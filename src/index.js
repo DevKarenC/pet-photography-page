cedd8e690873a6cc39ed102e8e06d1844e2d77b0
Karen <devkarenc@gmail.com>
2021-02-21 23:47:24 -0500

    Add pricing page and tab switching logic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ import appendHeader from "./modules/header";
 import home from "./modules/home";
 import pricing from "./modules/pricing";
 
-const contentDiv = document.querySelector("#content");
-// contentDiv.append(appendHeader(), home());
-contentDiv.append(appendHeader(), pricing());
+const contentDiv = document.querySelector(".content");
+const header = appendHeader();
+const homeDiv = home();
+const pricingDiv = pricing();
+contentDiv.append(header, homeDiv);
+
+const tabs = document.querySelectorAll("li");
+console.log(tabs);
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", changeTab);
+});
+
+function changeTab() {
+  if (this.textContent === "HOME") {
+    contentDiv.removeChild(contentDiv.lastChild);
+    contentDiv.append(homeDiv);
+    contentDiv.classList.remove("content-pricing", "content-contact");
+    contentDiv.classList.add("content");
+  } else if (this.textContent === "PRICING") {
+    contentDiv.removeChild(contentDiv.lastChild);
+    contentDiv.append(pricingDiv);
+    contentDiv.classList.remove("content", "content-contact");
+    contentDiv.classList.add("content-pricing");
+  }
+}
+
+// TODO: Attach event listener to each tab and call corresponding module
+// TODO: Also add/remove class for the contentDiv to replace background images
--- src/modules/pricing.js
+++ src/modules/pricing.js
@@ -1,42 +1,48 @@
 function pricing() {
   const pricingDiv = document.createElement("div");
   const prices = document.createElement("p");
-  const indoorPrice = document.createElement("span");
-  const outdoorPrice = document.createElement("span");
   const offerText = document.createElement("p");
+  offerText.textContent = "WHAT WE OFFER";
 
-  offerText.textContent = "What We Offer";
-  indoorPrice.textContent = "Indoor Studios $499";
-  outdoorPrice.textContent = "Outdoors $599";
-  prices.append(indoorPrice, outdoorPrice);
   prices.classList.add("prices");
   pricingDiv.classList.add("pricing");
-  pricingDiv.append(offerText, createCard(), prices);
+  pricingDiv.append(offerText, createCard(), priceButtons());
   return pricingDiv;
 }
 
 function createCard() {
-  const offer = document.createElement("div");
-  const offerListWithImages = {
-    "good-doggy.svg": "Lively and dedicated photo session up to 2 hours",
-    "dog-treat.svg": "Various toys, treats, and costumes for your pet",
-    "samples.svg": "Quality samples of photos for your best decision",
-    "online-gallery.svg": "Private, online gallery with 100 best images",
-    "camera.svg": "Seven professional photo prints with digital copies",
-    "memories.svg": "Lasting memories and adding colors to your moments",
-  };
-  offer.classList.add("offer");
-  for (const imgSource in offerListWithImages) {
-    const card = document.createElement("div");
-    const cardImg = `<img src="../src/images/${imgSource}" />`;
-    const cardDesc = `<p>${offerListWithImages[imgSource]}</p>`;
-    card.innerHTML += cardImg;
-    card.innerHTML += cardDesc;
-    card.classList.add("card");
-    offer.append(card);
-  }
-  return offer;
-  //   offer.innerHTML += offerList.map((item) => `<li>${item}</li>`).join("");
+  const offerDiv = document.createElement("div");
+  const offerList = [
+    "Lively and dedicated photo session up to 2 hours",
+    "Various toys, treats, and costumes for your pet",
+    "Quality samples of photos for your best decision",
+    "Private, online gallery with 100 best images",
+    "Seven professional photo prints with digital copies",
+    "Lasting memories and adding colors to your moments",
+  ];
+  offerDiv.classList.add("offer-div");
+  offerList.forEach((offer) => {
+    const offerLineDiv = document.createElement("div");
+    const offerLineP = document.createElement("p");
+    offerLineP.textContent = offer;
+    offerLineP.classList.add("offer-line-para");
+    offerLineDiv.append(offerLineP);
+    offerLineDiv.classList.add("offer-line-div");
+    offerDiv.append(offerLineDiv);
+  });
+  return offerDiv;
+}
+
+function priceButtons() {
+  const buttonsDiv = document.createElement("div");
+  const price = document.createElement("button");
+
+  price.textContent = "$499 per session";
+  price.classList.add("price-button");
+  buttonsDiv.classList.add("buttons-div");
+  buttonsDiv.append(price);
+
+  return buttonsDiv;
 }
 
 export default pricing;
